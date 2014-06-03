c6d9d2c02be61ee196093c9d74fc625121880a27
johnpauls <johnpauls@operamail.com>
2014-06-03 16:16:54 -0400

    Version 0.0.2 ui, server, global added

---
+++ ui.r
@@ -0,0 +1,16 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  
+  titlePanel(title="Veg Visualizer", windowTitle="Veg Vis"),
+  
+  
+  fluidRow(
+    column(3,
+      wellPanel(uiOutput("parkControl"))
+      ),
+    column(9,
+           plotOutput("TestIV")
+           )
+    )
+  ))
