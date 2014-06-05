a0ee1a7ee7b78bd9fe5f3fd639717fa40cad40ae
johnpauls <johnpauls@operamail.com>
2014-06-05 14:13:10 -0400

    version 0.2.2 added more densplot inputs

--- ui.r
+++ ui.r
@@ -5,21 +5,31 @@ library(NPSForVeg)
 
 shinyUI(
 
-  pageWithSidebar(
-
-    headerPanel(title="Veg Visualizer", windowTitle="Veg Vis"),
-  
-  
-  sidebarPanel(
-
-    uiOutput(outputId="ParkControl"),
+  fluidPage(
     
-     
-    sliderInput(inputId="YearIn", label="Display data from the 4 years ending:", min=2009, max=2013, value=2013, format="####")
+    titlePanel(title="Veg Visualizer", windowTitle="Veg Vis"),
+  
+    fluidRow(
+      column(3,
+          uiOutput(outputId="ParkControl"),
+          sliderInput(inputId="YearIn", label="Display data from the 4 years ending:", min=2009, max=2013, value=2013, format="####"),
+          sliderInput(inputId="TopIn", label="Number of species to display (in order of mean value):", min=1, max=10, value=5, format="##"),
+         hr(),
+         br(),
+          radioButtons(inputId="densvalues", label="Type of data to display", 
+                       choices=list("Abundance"="count","Size"="size","Occupancy"="presab") ),
+         hr(),
+         br(),
+         tags$div(title="Chose the type of plant you want to work with",
+                 radioButtons(inputId="densgroup", label="Type of plant",
+                       choices=c("trees","saplings","seedlings","herbs")))
+          
     ),
 
-    mainPanel(
+    column(9,
+
           plotOutput("Testdens")
+    )
     )
   
 ))
