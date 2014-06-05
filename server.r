8fc28a29c414f3907da824649b3f11ade6a3fbe9
johnpauls <johnpauls@operamail.com>
2014-06-05 14:13:10 -0400

    version 0.2.2 added more densplot inputs

--- server.r
+++ server.r
@@ -8,15 +8,20 @@ names(ParkList)<-getNames(NCRN)
 
 shinyServer(function(input, output){
   
-  output$ParkControl<-renderUI({expr=
-    selectInput(inputId="ParkIn",choices=ParkList, label="Park:") 
+  output$ParkControl<-renderUI({
+    selectizeInput(inputId="ParkIn",choices=ParkList, label="Park:",
+                   options = list(placeholder='Choose a park',
+                           onInitialize = I('function() { this.setValue(""); }') )) 
     })
-
-     
-
+   
+ 
   output$Testdens<-renderPlot({
-   print(densplot(NCRN[input$ParkIn], densargs=list(group="trees", years=c((input$YearIn-4):input$YearIn)), top=5, Total=F))
- })
-  
-  
+    print(
+      if (is.null(input$ParkIn) || nchar(input$ParkIn)==0) {return()}
+      else{
+        densplot(NCRN[input$ParkIn], densargs=list(group=input$densgroup, years=c((input$YearIn-3):input$YearIn),values=input$densvalues),
+                 top=input$TopIn, Total=F)
+      }
+    )
+  })
 })
