30f701d34a33a32aae4a7d8a8c1870ed7bd26035
johnpauls <johnpauls@operamail.com>
2014-06-16 16:25:49 -0400

    0.0.5 Map legend works, started Popups

--- server.r
+++ server.r
@@ -2,7 +2,6 @@ library(shiny)
 library(NPSForVeg)
 library(leaflet)
 library(lattice)
-#library(RColorBrewer)
 
 
 NCRN<-importNCRN("T:/I&M/MONITORING/Forest_Vegetation/RCode/VegData")
@@ -16,12 +15,10 @@ ParkBounds<-read.csv("boundboxes.csv", as.is=TRUE)
 
 shinyServer(function(input,output,session){
 
-######## main Map   
+################################## Code For Map Panel  ######################################################
+
+### Create Map  
 map<-createLeafletMap(session,"map")
-
-  
-
-
 
 #######  Park Filter for species list control for map
 
@@ -30,7 +27,6 @@ output$MapParkControl<-renderUI({
               choices=c(All="All",ParkList) )
 })
 
-
 ########### Species list control for map 
 output$MapSpeciesControl<-renderUI({
   if(is.null(input$MapPark) || nchar(input$MapPark)==0) {
@@ -38,12 +34,13 @@ output$MapSpeciesControl<-renderUI({
   }
   else{
     if(input$MapPark=="All"){
-    selectInput(inputId="MapSpecies", label="Choose a species", 
-             choices=sort(unique(getPlants(object=NCRN, group=input$MapGroup, years=c(2010:2013))$Latin_Name )) )
+    selectizeInput(inputId="MapSpecies", label="Choose a species", 
+             choices=c("All Species"="All",sort(unique(getPlants(object=NCRN, group=input$MapGroup, years=c(2010:2013))$Latin_Name))) )
     }
     else{
       selectInput(inputId="MapSpecies", label="Choose a species", 
-                  choices=sort(unique(getPlants(object=NCRN[input$MapPark], group=input$MapGroup, years=c(2010:2013))$Latin_Name )) )
+                  choices=c("All Species"="All",sort(unique(getPlants(object=NCRN[input$MapPark], group=input$MapGroup, 
+                                          years=c(2010:2013))$Latin_Name ))) )
     }
   }
 })
@@ -71,19 +68,39 @@ output$ParkZoomControl<-renderUI({
 })
 
 ###   Calculate the values for the circles on the map.
+MapSpeciesType<-reactive({ifelse(input$MapSpecies=="All", "Total","Individual") })
+MapSpeciesUse<-reactive({ifelse(input$MapSpecies=="All", NA, input$MapSpecies) })
 
-PlantVals<-reactive({
+#PlantVals<-reactive({
+#  if(is.null(input$MapSpecies) || nchar(input$MapSpecies)==0){
+#    return()
+#  }
+#  else{
+#    if(input$MapGroup != "herbs"){
+#      (10000/getArea(NCRN[1],group=input$MapGroup,type="all")) * (
+#      SiteXSpec(object=NCRN,group=input$MapGroup, years=c(2010:2013), species=MapSpeciesUse(), values=input$MapValues)$Total)
+#    }
+#    else{
+#      SiteXSpec(object=NCRN,group=input$MapGroup, years=c(2010:2013), species=MapSpeciesUse(), values=input$MapValues)$Total/12
+#    }
+#  }
+#})
+
+MapData<-reactive({
   if(is.null(input$MapSpecies) || nchar(input$MapSpecies)==0){
     return()
   }
   else{
-    if(input$MapGroup!="herbs"){
-      (10000/getArea(NCRN[1],group=input$MapGroup,type="all")) * (
-      SiteXSpec(object=NCRN,group=input$MapGroup, years=c(2010:2013), species=input$MapSpecies, values=input$MapValues,Total=F)[[2]])
-    }
-    else{
-      SiteXSpec(object=NCRN,group=input$MapGroup, years=c(2010:2013), species=input$MapSpecies, values=input$MapValues,Total=F)[[2]]/12
-    }
+    data.frame(getPlots(NCRN, years=c(2010:2013), output="dataframe")[c("Plot_Name","Unit_Code","Latitude","Longitude")],
+      Values=  
+        if(input$MapGroup != "herbs"){
+          (10000/getArea(NCRN[1],group=input$MapGroup,type="all")) * (
+          SiteXSpec(object=NCRN,group=input$MapGroup, years=c(2010:2013), species=MapSpeciesUse(), values=input$MapValues)$Total)
+        }
+        else{
+        SiteXSpec(object=NCRN,group=input$MapGroup, years=c(2010:2013), species=MapSpeciesUse(), values=input$MapValues)$Total/12
+        } 
+    )
   }
 })
 
@@ -91,15 +108,17 @@ PlantVals<-reactive({
 #MapObs<-return()
 session$onFlushed(once=TRUE, function() {   ##onFlushed comes superzip - makes map draw befrore circles
   MapObs<-observe({ 
+
     map$clearShapes()
-    try(                               #try deals wiht issue where the group has changed but spcies has not yet caught up.
-      if(is.null(PlantVals() )) {
+    try(                               #try deals with issue where the group has changed but species has not yet caught up.
+      if(is.null(MapData()$Values )) {
         return()
     }
     else {
-      map$addCircle(as.character(getPlots(NCRN)$Latitude), as.character(getPlots(NCRN)$Longitude), 15, 
-         options=list(color=BluePur(5)[cut(PlantVals(),breaks=c(MapLegend[[input$MapValues]][[input$MapGroup]]$Cuts), 
-                                           labels = FALSE)],fillOpacity=.7, weight=5) )
+      map$addCircle(as.character(MapData()$Latitude), as.character(MapData()$Longitude), 15, 
+         options=list(color=BluePur(5)[cut(MapData()$Values,
+          breaks=c(MapLegend[[MapSpeciesType()]][[input$MapValues]][[input$MapGroup]]$Cuts), 
+          labels = FALSE)],fillOpacity=.7, weight=5) )
     }
   )
 })
@@ -121,33 +140,59 @@ observe({
 })
 ######################  UIoutput for Legend
 output$MapLegend<-renderUI({
-  tags$table(
-  mapply(
-    function(BoxLabel,color){
-      tags$tr(tags$td(tags$div(
-        style=sprintf("width: 16px; height: 16px; background-color: %s;", color)
-      )),
-      tags$td(": ",BoxLabel)
-      )}, 
-    c(MapLegend[[input$MapValues]][[input$MapGroup]]$Labels),BluePur(5),SIMPLIFY=FALSE ))
+  if(is.null(input$MapSpecies) || nchar(input$MapSpecies)==0){
+    return()
+  }
+  else{
+    tags$table(
+    mapply(
+      function(BoxLabel,color){
+        tags$tr(tags$td(tags$div(
+          style=sprintf("width: 16px; height: 16px; background-color: %s;", color)
+        )),
+        tags$td(": ",BoxLabel)
+        )}, 
+      c(MapLegend[[MapSpeciesType()]][[input$MapValues]][[input$MapGroup]]$Labels),BluePur(5),SIMPLIFY=FALSE ))
+  }  
 })
-output$MapLegendTitle<-renderUI({h4(MapLegend[[input$MapValues]][[input$MapGroup]]$Title)})
-# Show a popup at the given location
-#showZipcodePopup <- function(zipcode, lat, lng) {
-#  selectedZip <- allzips[allzips$zipcode == zipcode,]
-#  content <- as.character(tagList(
-#    tags$h4("Score:", as.integer(selectedZip$centile)),
+
+output$MapLegendTitle<-renderUI({ 
+  if(is.null(input$MapSpecies) || nchar(input$MapSpecies)==0){
+    return()
+  }
+  else{
+    h4(MapLegend[[MapSpeciesType()]][[input$MapValues]][[input$MapGroup]]$Title) 
+  }
+})
+
+
+
+#Show a popup at the given location
+showPlotPopup <- function(Plot_Name, lat, lng) {
+  selectedPlot <- MapData()[MapData()$Plot_Name == Plot_Name,]
+  content <- as.character(tagList(
+   tags$h4("Park", selectedPlot$Unit_Code)
 #    tags$strong(HTML(sprintf("%s, %s %s",
 #                             selectedZip$city.x, selectedZip$state.x, selectedZip$zipcode
 #    ))), tags$br(),
 #    sprintf("Median household income: %s", dollar(selectedZip$income * 1000)), tags$br(),
 #    sprintf("Percent of adults with BA: %s%%", as.integer(selectedZip$college)), tags$br(),
 #    sprintf("Adult population: %s", selectedZip$adultpop)
-#  ))
-#  map$showPopup(lat, lng, content, zipcode)
-#}
-#
-#})
+  ))
+  map$showPopup(lat, lng, Plot_Name)
+}
+
+# When map is clicked, show a popup with city info
+clickObs <- observe({
+  map$clearPopups()
+  event <- input$map_shape_click
+  if (is.null(event))
+    return()
+  
+  isolate({
+    showPlotPopup(event$id, event$lat, event$lng)
+  })
+})
 
 
 ############ Park Control for Density plot  
--- ui.r
+++ ui.r
@@ -4,36 +4,33 @@ library(shiny)
 library(NPSForVeg)
 
 shinyUI(
- 
-
-
   navbarPage(title="Forest Vegetation Visualizer", windowTitle="Forest Veg",
-             
+  
     tabPanel("Map",
-             div(class="outer",
-                 tags$head(
-                   includeCSS("mapstyles.css")
-                   ),
-                 
-                 leafletMap("map", width="100%", height="100%",
-                            initialTileLayer="//{s}.tiles.mapbox.com/v3/nps.2yxv8n84/{z}/{x}/{y}.png",
-                            initialTileLayerAttribution = HTML( "&copy; <a href='http://mapbox.com/about/maps' target='_blank'>Mapbox</a> &copy; <a href='http://openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors | <a class='improve-park-tiles' href='http://www.nps.gov/npmap/park-tiles/improve/' target='_blank'>Improve Park Tiles</a>"),
+      div(class="outer",
+        tags$head(
+          includeCSS("mapstyles.css")
+        ),
+        leafletMap("map", width="100%", height="100%",initialTileLayer="//{s}.tiles.mapbox.com/v3/nps.2yxv8n84/{z}/{x}/{y}.png",
+          initialTileLayerAttribution = HTML("&copy; <a href='http://mapbox.com/about/maps' target='_blank'>Mapbox</a> 
+          &copy; <a href='http://openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors | 
+          <a class='improve-park-tiles' href='http://www.nps.gov/npmap/park-tiles/improve/' target='_blank'>Improve Park Tiles</a>"),
                                                                 #'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
-                            options=list(
-                              center = c(39.03, -77.80),
-                              zoom = 9,
-                              maxBounds = list(list(37.70,-79.5), list(40.36,-76.1)), # Show NCRN only
-                              minZoom=8
-                            )
-                 ),
+        options=list(
+          center = c(39.03, -77.80),
+          zoom = 9,
+          maxBounds = list(list(37.70,-79.5), list(40.36,-76.1)), # Show NCRN only
+          minZoom=8
+        )
+      ),
                  
                  fixedPanel(id="controls",class="modal",draggable=TRUE,cursor="auto",top=50,bottom="auto",height="auto",right=20, 
                             left="auto",width=200,
                             h3("Forest Explorer"),
-                            
                             selectInput(inputId="MapGroup", label="Type of plant:",
                                                   choices=c(Trees="trees",Saplings="saplings","Tree seedlings"="seedlings",Shrubs="shrubs",
-                                                            "Shrub seedlings"="shseedlings", "Understory plants"="herbs",Vines="vines")),
+                                                            "Shrub seedlings"="shseedlings", "Understory plants"="herbs",
+                                                            "Vines on Trees"="vines")),
                             
                             uiOutput("PlantValueControl"),
                             
@@ -47,10 +44,11 @@ shinyUI(
                             h4("Zoom to:"),
                             uiOutput("ParkZoomControl"),
                             actionButton(inputId="MapZoom", label="Go",icon=icon("search-plus"))
+       
                  ),
                  
-                 fixedPanel( id="controls", class="floater",style="", draggable=TRUE, cursor="auto", top=500, bottom="auto", height=135,
-                             right=100, left="auto", width=100,
+                 fixedPanel( id="controls", class="floater",style="", draggable=TRUE, cursor="auto", top=50, bottom="auto", height=135,
+                             right=300, left="auto", width=150,
                              uiOutput("MapLegendTitle"),
                              uiOutput("MapLegend")
                 ) 
