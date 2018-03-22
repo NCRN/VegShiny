54736442911a5ece035b3107efdd34b017ab2792
johnpauls <johnpauls@operamail.com>
2018-03-22 13:59:33 -0400

    v 1.3.1
    - added SHEN information html
    - added SHEN Citations html
    - updated swithces in global.r
    
    - Closes #19

--- global.r
+++ global.r
@@ -44,7 +44,7 @@ ProjectInfo<-switch(Network,
                     MIDN=includeHTML("./www/InformationMIDN.html"),
                     NCRN=includeHTML("./www/Information.html"),
                     NETN=includeHTML("./www/InformationNETN.html"),
-                    SHEN=HTML("<h1>Add Me!</h1>")
+                    SHEN=HTML("./www/InformationSHEN.html")
 )
 
 Citations<-switch(Network,
@@ -52,7 +52,7 @@ Citations<-switch(Network,
                     MIDN=includeHTML("./www/CitationsMIDN.html"),
                     NCRN=includeHTML("./www/Citations.html"),
                     NETN=includeHTML("./www/CitationsNETN.html"),
-                    SHEN=HTML("<h1>Add Me!</h1>")
+                    SHEN=HTML("./www/CitationsSHEN.html")
 )
 
 
