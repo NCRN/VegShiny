f68efe634555a01683ee0896a31c999268a455bc
charlotte clark <charlotte_clark@partner.nps.gov>
2026-03-09 09:03:32 -0400

    Updated R version

---
+++ dev.R
@@ -0,0 +1,60 @@
+# create the dev environment from scratch
+
+options(download.file.method = "wininet")
+# wd <- getwd()
+# unlink(file.path(wd, 'renv'), recursive = TRUE, force=T)
+# unlink(file.path(wd, 'renv.lock'), recursive = TRUE, force=T)
+library(renv)
+renv::init()
+default_dependencies <- c(
+    # packages that consistently cause problems for me
+    'usethis'
+    ,'fs'
+    ,'languageserver'
+    ,'magrittr'
+    ,'purrr'
+    ,'jsonlite'
+    ,'knitr'
+    ,'rlang'
+    ,'ggplot2'
+    ,'glue'
+    ,'pillar'
+    ,'tibble'
+    ,'stringr'
+    ,'data.table'
+    ,'readr'
+    ,'tidyr'
+    ,'ritis'
+    ,'taxize'
+    ,'covr'
+    ,'EML'
+    ,'httr'
+    ,'worrms'
+    ,'devtools'
+    ,'remotes'
+    ,'gargle'
+    ,'ids'
+    ,'readxl'
+    ,'curl'
+    ,'EML'
+    ,'rlang'
+    ,'tidyverse'
+    ,'ISOcodes'
+    ,'ellipsis'
+    ,'gtable'
+    ,'scales'
+    ,'desc'
+    ,'shiny'
+    ,'leaflet'
+    ,'lattice'
+    ,'shinyjs'
+    ,'DT'
+    ,'sf'
+)
+renv::install(default_dependencies, prompt=F,rebuild=T)
+
+library(remotes)
+remotes::install_github('NCRN/NPSForVeg', force=T, upgrade='never')
+library(NPSForVeg)
+renv::snapshot(prompt = F)
+renv::status()
